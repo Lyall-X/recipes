bff0a1f26e4732b6b6d8fc1b1738aaf84fc5a9aa
Lyall <lyall.liu@gmail.com>
2024-10-15 22:02:39 +0800

    add chapter 5

--- cpp_feature/effective_c++/e_chapter_5.cc
+++ cpp_feature/effective_c++/e_chapter_5.cc
@@ -65,6 +65,40 @@ void TestFunc(MyString&& str)
     std::cout << "rvalue: " << str.str_ << std::endl;
 }
 
+// T&&: 万能引用, 因为T可能是A&, A&&, const A&, const A&&, 本质是对T进行推到后，产生引用折叠
+// 完美转发这么用：获取原始的左值或右值
+// 在右值引用上使用std::move，在通用引用上使用std::forward
+//    - 因为右值作为形参，会成为左值，要么std::move转换为右值，要么std::forward转发为右值
+//    - 因为T&&是万能引用，所以T&&是左值，std::forward<T>能使其保持原始类型
+//    - 1.如果 T&& 使用 std::move, 会导致左值成未定义的行为 2.如果 Widget&& 使用 std::forward, 代码很长不利于阅读
+template <typename A, typename B>
+void ForwardFunc(A&& a, B&& b)
+{
+    // success
+    std::string str_a = std::forward<A>(a);
+    std::string str_b = std::forward<B>(b);
+    /* failure
+    std::string str_c = std::move(a);
+    */
+}
+
+void ForwardFunc2(auto&& str)
+{
+    // success
+    MyString str1 = std::move(str);
+    // success
+    MyString str2 = std::forward<MyString>(str);
+}
+
+// Widget&& w: 右值引用，但是是个左值
+void f(MyString&& w)
+{
+    // 1.
+    MyString w1 = std::move(w);
+    // 2.
+    MyString w2 = std::forward<MyString>(w);
+}
+
 int main()
 {
     // MyString str("hello");
