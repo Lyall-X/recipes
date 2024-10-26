77583a07250caace8f80416f26742093828980ff
Lyall <lyall.liu@gmail.com>
2024-10-26 15:23:50 +0800

    submit template

--- cpp_feature/template/chapter1.cc
+++ cpp_feature/template/chapter1.cc
@@ -1,4 +1,124 @@
+/**
+ * @brief
+ * 1.类型与对象
+ * Param{} 比 Param() 更加安全，因为后者可能被解释为函数声明: 统一初始化语法（包含函数对象）
+ * 递归过程中, 右值引用可以作为中间结果进行修改
+ */
+#include <cstdio>
+#include <iostream>
+#include <vector>
+struct Param
+{
+    Param() { printf("Param()\n"); }
+    Param(const Param &p)
+    {
+        (void)p;
+        printf("Param(const Param &p)\n");
+    }
+    Param(Param &&p)
+    {
+        (void)p;
+        printf("Param(Param &&p)\n");
+    }
+};
+
+void f(Param &p)
+{
+    (void)p;
+    printf("f(Param &p)\n");
+}
+
+void f(const Param &p)
+{
+    (void)p;
+    printf("f(const Param &p)\n");
+}
+
+void f(Param &&p)
+{
+    (void)p;
+    printf("f(Param &&p)\n");
+}
+
+template <typename T>
+void f(T &&t)
+{
+    (void)t;
+    printf("f(T &&t)\n");
+}
+
+// decltype: 获取值类型： 1.不带括号，获取定义时类型 2.带括号，获取作为参数的类型
+// 推到函数返回值: 需要实例化出对象, decltype(f(Param{}))
+#include <functional>
+#include <typeinfo>
+// 推到函数返回值
+// declvalue: 由类型推导出对象: 1.不需要实例化 2.不受构造函数限制
+template <typename F, typename... Args>
+using FuncResultType = decltype(F{}(Args{}...));
+
+template <typename F, typename... Args>
+using FuncResultType2 = decltype(std::declval<F>()(std::declval<Args>()...));
+
+// 值语义多态: std::variant, std::visit -> 双重派发技术
+#include <variant>
+struct VarA
+{
+    void Print() { printf("VarA\n"); }
+};
+
+struct VarB
+{
+    void Print() { printf("VarB\n"); }
+};
+
+// 打印模板类型: 1.利用警告打印  2.利用模板函数名打印
+// template <typename... Ts>
+// struct [[deprecated]] dump
+// {};
+
+template <typename... Ts>
+void dump()
+{
+    printf("T: %s\n", __PRETTY_FUNCTION__);
+};
+
+#if 1
+int main()
+{
+    // && > const &
+    f(Param{});
+    Param p;
+    // & > const &
+    f(p);
+    // lambda默认为 constexpr
+    constexpr auto add = [](int a, int b) { return a + b; };
+    printf("add: %d\n", add(1, 2));
+    // 泛型lambda
+    auto add2 = [](auto a, auto b) { return a + b; };
+    auto add3 = []<typename T>(T a, T b) { return a + b; };
+    // bind
+    int value = 1;
+    std::bind(add, std::ref(value), 2);
+
+    using Var = std::variant<VarA, VarB>;
+    Var a     = VarA{};
+    Var b     = VarB{};
+    std::visit([](auto &&v) { v.Print(); }, a);
+    std::visit([](auto &&v) { v.Print(); }, b);
+
+    // 打印模板类型
+    // dump<int, VarA, VarB> d;
+    dump<Var, int>();
+
+    return 0;
+}
+#endif
+
+// ####################################################################################
+
+#if 0
 int main()
 {
     return 0;
 }
+#endif
---
+++ cpp_feature/template/chapter2.cc
@@ -0,0 +1,110 @@
+#include <cstdio>
+#include <cstdlib>
+
+template <typename T>
+void PT()
+{
+    printf("T: %s\n", __PRETTY_FUNCTION__);
+}
+
+// 1.ADL: 非限定名称查找下生效
+void func(int){};
+/**
+1. 静态成员变量  T::value, 类型别名 typename T::value(c++20放松了限制)
+2. 模板元编程 -> 优于 SFINAE
+3. 函数重载不考虑返回值: 因为返回值可以不处理
+*/
+
+// ################## 1. declvalue ##################
+// 右值引用只涉及类型引用的绑定，并不实际创建对象
+#include <type_traits>
+template <typename T>
+T declvalue(long);  // 只有void匹配
+
+template <typename T, typename U = T&&>
+U declvalue(int);  // 非void优先匹配
+// 辅助模板用于延迟触发 assert_assert, 同时避免declvalue求值
+template <typename T>
+struct declvalue_assist : std::false_type
+{};
+template <typename T>
+auto declvalue() noexcept -> decltype(declvalue<T>(0))  // 可选，辅助编译器推导返回值
+{
+    static_assert(declvalue_assist<T>::value, "declvalue: 只能用于类型推导");
+    return declvalue<T>(0);
+}
+
+// ################## 2. type traits ##################
+template <typename T, typename U>
+struct is_same : std::false_type  // 能匹配所有类型, false
+{};
+template <typename T>
+struct is_same<T, T> : std::true_type  // 偏特化类型, true
+{};
+
+template <bool, typename T = void>  // T 没有使用可以缺省
+struct enable_if                    // 没有type替换失败
+{};
+
+template <typename T>
+struct enable_if<true, T>
+{
+    using type = T;  // 其实T是void，但是额外type的成员
+};
+
+// 类型自省: 结构拆分
+// 1.实现通用模板，防止编译不过
+template <typename T>
+struct function_traits
+{};
+// 2.特化模板: a.函数类型 b.剖析函数结构
+#include <utility>
+template <typename R, typename... Args>
+struct function_traits<R(Args...)>
+{
+    using return_type                   = R;
+    using args_type                     = std::tuple<Args...>;
+    static constexpr size_t num_of_args = sizeof...(Args);
+    template <size_t I>
+    using arg = std::tuple_element_t<I, args_type>;
+};
+// ################## 3. type traits ##################
+// 标签分发: 函数重载
+// if constexpr 判断
+// template <class...> using void_t = void; 用于判断 <> 里的类型是否良构
+
+int main()
+{
+    // 不采用ADL
+    (func)(1);
+
+    // declvalue 类型右值引用，返回的是value
+    decltype(declvalue<int>()) a = 1;
+    int&& b                      = 1;
+    PT<decltype(declvalue<int>())>();    // [T = int &&] 表达式右值引用
+    PT<decltype((declvalue<int>()))>();  // [T = int &&] 表达式右值引用
+    PT<decltype(b)>();                   // [T = int &&] 右值
+    PT<decltype((b))>();                 // [T = int &]  表达式左值
+
+    // type traits: type check
+    static_assert(std::is_integral_v<int>);
+    static_assert(std::is_floating_point_v<float>);
+    static_assert(std::is_same_v<int, int>);
+    static_assert(!std::is_class_v<int>);
+    // type traits: type transform
+    static_assert(std::is_same_v<std::remove_const_t<const int>, int>);
+    static_assert(std::is_same_v<std::add_const_t<int>, const int>);
+    static_assert(std::is_same_v<std::remove_reference_t<int&>, int>);
+    static_assert(std::is_same_v<std::remove_reference_t<int&&>, int>);
+    static_assert(std::is_same_v<std::add_pointer_t<int>, int*>);
+    static_assert(std::is_same_v<std::decay_t<int[10]>, int*>);
+    // type traits: assist template
+    using warp = std::integral_constant<int, 100>;  // type, value_type 成员
+    static_assert(warp::value == 100);
+    static_assert(std::is_same_v<warp::value_type, int>);
+    using false_warp = std::integral_constant<bool, false>;
+    static_assert(!false_warp::value);
+    // 空基类优化: 继承空基类为0字节 or 组合用 [[no_unique_address]]
+
+    return 0;
+}
