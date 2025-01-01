436e9ca5061ba1fd38d0aac6d0c4cd78aab18ad0
lyall <lyall.liu@gmail.com>
2025-01-01 13:28:32 +0800

    [add] add new ebpf

---
+++ projects/ebpf/ebpf.cc
@@ -0,0 +1,13 @@
+#include <dlfcn.h>
+
+#include <chrono>
+#include <cstdio>
+#include <functional>
+#include <thread>
+
+int main()
+{
+    printf("begin\n");
+    printf("end\n");
+    return 0;
+}
